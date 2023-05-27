e2f82248fccb45c3d1ba5baff65cfaadea8286d4
MouchardDamien <133344364+MouchardDamien@users.noreply.github.com>
2023-05-27 12:37:03 +0200

    Update game.c

--- game.c
+++ game.c
@@ -1,16 +1,16 @@
 #include "header.h"
 
-
+//Fonction game2
+//Fonction
 void game2(Player * playerA, Player * playerB, T_Element  plateau[SIZE_PLATEAU][SIZE_PLATEAU], int nb_player, T_Element tab_elements[], char name_elements[14][11], char name_role1[NB_ROLE][10]){
   int player_not_win = 0;
   while(player_not_win != 1){
 
     printf("\n");
-    player_not_win = player_turn(nb_player, playerA, plateau, tab_elements, name_elements, name_role1);
-    
+    player_not_win = player_turn(nb_player, playerA, plateau, tab_elements,   name_elements, name_role1);
     
     printf("\n");
-    if(player_not_win != 1){
+    if(player_not_win != 1){ //Verifie si le joueur a gagne
       ClearScreen();
       player_not_win = player_turn(nb_player, playerB, plateau, tab_elements, name_elements, name_role1);
     }
@@ -18,6 +18,7 @@ void game2(Player * playerA, Player * playerB, T_Element  plateau[SIZE_PLATEAU][
 }
 
 
+//Fonction game3
 void game3(Player * playerA, Player * playerB, Player * playerC, T_Element plateau[SIZE_PLATEAU][SIZE_PLATEAU], int nb_player, T_Element tab_elements[], char name_elements[14][11], char name_role2[NB_ROLE][10]){
   int player_not_win = 0;
   while(player_not_win != 1){
@@ -26,21 +27,21 @@ void game3(Player * playerA, Player * playerB, Player * playerC, T_Element plate
     player_not_win = player_turn(nb_player, playerA, plateau, tab_elements, name_elements, name_role2);
     
     printf("\n");
-    if(player_not_win != 1){
+    if(player_not_win != 1){ //Verifie si le joueur a gagne
       ClearScreen();
       player_not_win = player_turn(nb_player, playerB, plateau, tab_elements, name_elements, name_role2);
     }
     
     
     printf("\n");
-    if(player_not_win != 1){
+    if(player_not_win != 1){ //Verifie si le joueur a gagne
       ClearScreen();
       player_not_win = player_turn(nb_player, playerC, plateau, tab_elements, name_elements, name_role2);
     }
   }
 }
 
-
+//Fonction game4
 void game4(Player * playerA, Player * playerB, Player * playerC, Player * playerD, T_Element plateau[SIZE_PLATEAU][SIZE_PLATEAU], int nb_player, T_Element tab_elements[], char name_elements[14][11], char name_role3[NB_ROLE][10]){
   int player_not_win = 0;
   while(player_not_win != 1){
@@ -50,20 +51,20 @@ void game4(Player * playerA, Player * playerB, Player * playerC, Player * player
     
     
     printf("\n");
-    if(player_not_win != 1){
+    if(player_not_win != 1){ //Verifie si le joueur a gagne
       ClearScreen();
       player_not_win = player_turn(nb_player, playerB, plateau, tab_elements, name_elements, name_role3);
     }
     
     
     printf("\n");
-    if(player_not_win != 1){
+    if(player_not_win != 1){ //Verifie si le joueur a gagne
       ClearScreen();
       player_not_win = player_turn(nb_player, playerC, plateau, tab_elements, name_elements, name_role3);
     }
     
     printf("\n");
-    if(player_not_win != 1){
+    if(player_not_win != 1){ //Verifie si le joueur a gagne
       ClearScreen();
       player_not_win = player_turn(nb_player, playerD, plateau, tab_elements, name_elements, name_role3);
     }
