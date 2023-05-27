6f5d983801dd392e6bf1bf9fc6fc4e9107b5ade4
MouchardDamien <133344364+MouchardDamien@users.noreply.github.com>
2023-05-27 21:40:06 +0200

    Update game.c

--- game.c
+++ game.c
@@ -1,7 +1,7 @@
 #include "header.h"
 
 //Fonction game2
-//Fonction
+//Permet de lancer la partie pour 2 joueurs
 void game2(Player * playerA, Player * playerB, T_Element  plateau[SIZE_PLATEAU][SIZE_PLATEAU], int nb_player, T_Element tab_elements[], char name_elements[14][11], char name_role1[NB_ROLE][10]){
   int player_not_win = 0;
   while(player_not_win != 1){
@@ -19,6 +19,7 @@ void game2(Player * playerA, Player * playerB, T_Element  plateau[SIZE_PLATEAU][
 
 
 //Fonction game3
+//Permet de lancer la partie pour 3 joueurs
 void game3(Player * playerA, Player * playerB, Player * playerC, T_Element plateau[SIZE_PLATEAU][SIZE_PLATEAU], int nb_player, T_Element tab_elements[], char name_elements[14][11], char name_role2[NB_ROLE][10]){
   int player_not_win = 0;
   while(player_not_win != 1){
@@ -42,6 +43,7 @@ void game3(Player * playerA, Player * playerB, Player * playerC, T_Element plate
 }
 
 //Fonction game4
+//Permet de lancer la partie pour 4 joueurs
 void game4(Player * playerA, Player * playerB, Player * playerC, Player * playerD, T_Element plateau[SIZE_PLATEAU][SIZE_PLATEAU], int nb_player, T_Element tab_elements[], char name_elements[14][11], char name_role3[NB_ROLE][10]){
   int player_not_win = 0;
   while(player_not_win != 1){
