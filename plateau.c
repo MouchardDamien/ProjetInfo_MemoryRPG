3d6f286f644befee4b9788991d2c4030a58b7066
MouchardDamien <133344364+MouchardDamien@users.noreply.github.com>
2023-05-27 21:41:52 +0200

    Update plateau.c

--- plateau.c
+++ plateau.c
@@ -1,12 +1,13 @@
 #include "header.h"
 
-
+//Fonction create_plateau
+//Permet de creer un plateau 5*5 de facon aleatoire
 void create_plateau(T_Element tab_plateau[SIZE_PLATEAU][SIZE_PLATEAU], T_Element tab_Elements1[]) {
   int index;
   for (int i = 0; i < SIZE_PLATEAU; i++) {
     for (int j = 0; j < SIZE_PLATEAU; j++) {
       int used = 1;
-      while (used) {
+      while (used) { //On verifie qu'un element n'ai pas deja ete pris pour pouvoir le mettre dans le plateau
         index = rand() % 25;
         if (tab_Elements1[index] != USED) {
           tab_plateau[i][j] = tab_Elements1[index];
@@ -18,7 +19,8 @@ void create_plateau(T_Element tab_plateau[SIZE_PLATEAU][SIZE_PLATEAU], T_Element
   }
 }
 
-
+//Fonction create_hiden_plateau
+//Permet de creer un plateau 5*5 avec seulement des cases cachees
 void create_hiden_plateau(T_Element tab[SIZE_PLATEAU][SIZE_PLATEAU], T_Element tab_Elements2[]) {
   for (int i = 0; i < SIZE_PLATEAU; i++) {
     for (int j = 0; j < SIZE_PLATEAU; j++) {
@@ -27,15 +29,16 @@ void create_hiden_plateau(T_Element tab[SIZE_PLATEAU][SIZE_PLATEAU], T_Element t
   }
 }
 
-
+//Fonction print_Plateau
+//Permet d'afficher un plateau
 void print_Plateau(int nb_p, T_Element tab_plateau[SIZE_PLATEAU][SIZE_PLATEAU], char name_elements[14][11]) {
   printf("\n\n");
-  printf("           \U0001F7E9 \n"); //carre vert
+  printf("           \U0001F7E9 \n"); 
   for (int i = 0; i < SIZE_PLATEAU; i++) {
     for (int j = 0; j < (SIZE_PLATEAU); j++) {
       if (j == 0) {
         if (i == 1) {
-          printf("\U0001F7E6"); //carre bleu
+          printf("\U0001F7E6"); 
         } else {
           printf("  ");
         }
@@ -45,20 +48,21 @@ void print_Plateau(int nb_p, T_Element tab_plateau[SIZE_PLATEAU][SIZE_PLATEAU], 
                                                 // chaine de caractere
       if (j == 4 && i == 3) {
         if(nb_p > 2){
-          printf("\U0001F7E5"); //carre rouge
+          printf("\U0001F7E5"); 
         }
       }
     }
     printf("\n");
   }
   if(nb_p > 3){
-    printf("     \U0001F7E8 \n"); //carre jaune
+    printf("     \U0001F7E8 \n"); 
   }
   printf("\n\n");
 }
 
 
-
+//Fonction name
+//Permet d'afficher le nom du joueur quand c'est son tour de jouer ainsi que sa couleur avec les emojis unicode
 void name(Player * A){
 
   switch(A->num){
