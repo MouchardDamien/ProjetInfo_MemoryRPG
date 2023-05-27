4f09f810a36213e3fa229f8d996b49476b7293f2
MouchardDamien <133344364+MouchardDamien@users.noreply.github.com>
2023-05-27 21:42:11 +0200

    Update utility.c

--- utility.c
+++ utility.c
@@ -1,20 +1,23 @@
 #include "header.h"
 
 
-
+//Fonction vide buffer
+//Utiliser pour securiser et verifier les scanf
 void vide_buffer(){
 	while(getchar()!='\n'){
 	}
 }
 
 
-
+//Fonction ClearScreen
+//Utiliser pour l'affichage graphique --> supprime la page pour laisser les nouvelles instructions au propre
 void ClearScreen(){ 
   printf("\033[H\033[J");
 }
 
 
-
+//Fonction chrono
+//Permet de recuperer le temps selon le moment ou la fonction est lance
 time_t chrono(){
   time_t ta;
   time (&ta);
